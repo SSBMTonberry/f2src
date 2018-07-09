13f0b1233a97c6d1bf2e38d90cda66f22cf4112e
Robin Pettersen <ssbmtonberry@gmail.com>
2018-07-09 17:26:21 +0200

    Auto stash before merge of "master" and "origin/master"
    Also added a file for readme

--- AssetGeneratorConfig.h
+++ AssetGeneratorConfig.h
@@ -1,13 +1,13 @@
 #define VERSION_MAJOR 1
 #define VERSION_MINOR 0
 #define VERSION_PATCH 0
-#define LINUX 0
-#define WINDOWS 1
+#define LINUX 1
+#define WINDOWS 0
 #define APPLE 0
-#define MSVC 1
+#define MSVC 0
 #define MINGW 0
-#define CMAKE_MAKE_PROGRAM C:/Program Files (x86)/Microsoft Visual Studio/2017/Enterprise/MSBuild/15.0/Bin/MSBuild.exe
-#define CMAKE_SYSTEM_NAME Windows
-#define CMAKE_HOST_SYSTEM_NAME Windows //Windows, Linux or Darwin (OS X)
-#define CMAKE_HOST_SYSTEM_PROCESSOR AMD64
-#define CMAKE_CXX_COMPILER_VERSION 19.14.26431.0
+#define CMAKE_MAKE_PROGRAM /usr/bin/make
+#define CMAKE_SYSTEM_NAME Linux
+#define CMAKE_HOST_SYSTEM_NAME Linux //Windows, Linux or Darwin (OS X)
+#define CMAKE_HOST_SYSTEM_PROCESSOR x86_64
+#define CMAKE_CXX_COMPILER_VERSION 8.1.1
